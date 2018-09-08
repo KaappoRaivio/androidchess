735b5e43d972746949087a1f2d845a3bd72ec619
NoSocksForYou <kaappo.raivio@gmail.com>
2018-09-08 10:55:50 +0300

    mainactivity done

--- androidchess/app/src/main/java/kaappo/androidchess/MainActivity.java
+++ androidchess/app/src/main/java/kaappo/androidchess/MainActivity.java
@@ -2,6 +2,8 @@ package kaappo.androidchess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        // <Set content of @id/i_play_as>
+//        Spinner spinner = (Spinner) findViewById(R.id.i_play_as);
+//
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.activity_main_white_and_black,
+//                android.R.layout.simple_spinner_item);
+//
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//        // </Set content of @id/i_play_as>
     }
 }
