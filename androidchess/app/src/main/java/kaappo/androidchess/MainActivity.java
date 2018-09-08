c2b3e312f7f0b7ea1e587923d0fd10790d1a3881
NoSocksForYou <kaappo.raivio@gmail.com>
2018-09-08 11:29:49 +0300

    as

--- androidchess/app/src/main/java/kaappo/androidchess/MainActivity.java
+++ androidchess/app/src/main/java/kaappo/androidchess/MainActivity.java
@@ -1,26 +1,40 @@
 package kaappo.androidchess;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final String PLAYER_SIDE = "a";
+    public final String COMPUTER_LEVEL = "b";
+
+    public final String BUNDLE_KEY = "c";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        // <Set content of @id/i_play_as>
-//        Spinner spinner = (Spinner) findViewById(R.id.i_play_as);
-//
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-//                R.array.activity_main_white_and_black,
-//                android.R.layout.simple_spinner_item);
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-//        // </Set content of @id/i_play_as>
+    }
+
+    public void onPlayButtonClick (View button) {
+        Spinner sideSpinner = (Spinner) findViewById(R.id.i_play_as_spinner);
+        String playerSide = sideSpinner.getSelectedItem().toString();
+
+        Spinner levelSpinner = (Spinner) findViewById(R.id.opponent_level_spinner);
+        String computerLevel = levelSpinner.getSelectedItem().toString();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(PLAYER_SIDE, playerSide);
+        bundle.putString(COMPUTER_LEVEL, computerLevel);
+
+        Intent intent = new Intent(getApplicationContext(), ChessActivity.class);
+        intent.putExtra(BUNDLE_KEY, bundle);
+        startActivity(intent);
+
     }
 }
