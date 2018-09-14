949c283f3cadea1ec3b34cd663ff1d16b845c2f0
NoSocksForYou <kaappo.raivio@gmail.com>
2018-09-14 22:46:14 +0300

    väsynyttä menoa

--- androidchess/app/src/main/java/kaappo/androidchess/ChessActivity.java
+++ androidchess/app/src/main/java/kaappo/androidchess/ChessActivity.java
@@ -1,15 +1,30 @@
 package kaappo.androidchess;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewParent;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ChessActivity extends AppCompatActivity {
+
+    public static String getId(View view) {
+        if (view.getId() == 0xffffffff) return "no-id";
+        else return view.getResources().getResourceName(view.getId());
+    }
+
+    public int getViewIDByString (String name) {
+        Resources res = getResources();
+        return res.getIdentifier(name, "id", getPackageName());
+
+    }
 
     public static boolean isPlayersTurn;
 
@@ -66,5 +81,62 @@ public class ChessActivity extends AppCompatActivity {
     public String getMove () {
         return "";
     }
+
+    public RelativeLayout getSquareByPosition (int pos_x, int pos_y) throws Exception {
+        char letter;
+
+        switch (pos_x) {
+            case 0:
+                letter = 'A';
+                break;
+            case 1:
+                letter = 'B';
+                break;
+            case 2:
+                letter = 'C';
+                break;
+            case 3:
+                letter = 'D';
+                break;
+            case 4:
+                letter = 'E';
+                break;
+            case 5:
+                letter = 'F';
+                break;
+            case 6:
+                letter = 'G';
+                break;
+            case 7:
+                letter = 'H';
+                break;
+            default:
+                throw new Exception("invalid pos_x of " + pos_x);
+        }
+
+        try {
+            return (RelativeLayout) findViewById(getViewIDByString(letter + String.valueOf(pos_y + 1)));
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+    public ImageView getPieceByPosition (int pos_x, int pos_y) throws Exception {
+        try{
+            return (ImageView) getSquareByPosition(pos_x, pos_y).getChildAt(0);
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    public void changeParentView (RelativeLayout oldParent, View view, RelativeLayout newParent) {
+        oldParent.removeView(view);
+        newParent.addView(view);
+
+        return;
+    }
+
+
 
 }
--- androidchess/app/src/main/java/kaappo/androidchess/askokaappochess/AndroidUI.java
+++ androidchess/app/src/main/java/kaappo/androidchess/askokaappochess/AndroidUI.java
@@ -3,6 +3,12 @@ package kaappo.androidchess.askokaappochess;
 
 import android.app.Activity;
 import android.content.Context;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kaappo.androidchess.ChessActivity;
 
 public class AndroidUI {
     int squares[][];
@@ -14,10 +20,10 @@ public class AndroidUI {
 
     int turn = -1;
 
-    private Activity context;
+    private ChessActivity context;
 
-    public AndroidUI (chessboard cb, Activity context) {
-        this.squares = new int[9][9];
+    public AndroidUI (chessboard cb, ChessActivity context) {
+        this.squares = new int[8][8];
         this.chessboard = cb;
 
         this.context = context;
@@ -45,7 +51,90 @@ public class AndroidUI {
     }
 
     public void updateData (chessboard cb) {
+        this.chessboard = cb;
 
+        for (int x = 1; x <= 8; x++) {
+            for (int y = 1; y <= 8; y++) {
+                this.squares[x][y] = cb.piecevalue(x, y);
+            }
+        }
+
+
+    }
+    public void updateBoard () throws Exception{
+        List<Integer> alreadyMovedPieces = new ArrayList<>();
+        List<ImageView> unusedViews = new ArrayList<>();
+
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                if (context.getPieceByPosition(x, y) != null) {
+                    unusedViews.add(context.getPieceByPosition(x, y));
+                }
+            }
+        }
+
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                int square = this.squares[x][y];
+
+                ImageView toBePlaced;
+                if (square == -1)
+                    continue;
+                else if (square < 100) {
+
+                    switch (square - 100) {
+                        case piece.PAWN:
+                            toBePlaced = searchPiece(unusedViews, "white_pawn");
+                            break;
+                        case piece.BISHOP:
+                            toBePlaced = searchPiece(unusedViews, "white_bishop");
+                            break;
+                        case piece.KNIGHT:
+                            toBePlaced = searchPiece(unusedViews, "white_knight");
+                            break;
+                        case piece.ROOK:
+                            toBePlaced = searchPiece(unusedViews, "white_rook");
+                            break;
+                        case piece.QUEEN:
+                            toBePlaced = searchPiece(unusedViews, "white_queen");
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                } else {
+                    switch (square) {
+                        case piece.PAWN:
+                            toBePlaced = searchPiece(unusedViews, "black_pawn");
+                            break;
+                        case piece.BISHOP:
+                            toBePlaced = searchPiece(unusedViews, "black_bishop");
+                            break;
+                        case piece.KNIGHT:
+                            toBePlaced = searchPiece(unusedViews, "black_knight");
+                            break;
+                        case piece.ROOK:
+                            toBePlaced = searchPiece(unusedViews, "black_rook");
+                            break;
+                        case piece.QUEEN:
+                            toBePlaced = searchPiece(unusedViews, "black_queen");
+                            break;
+                        default:
+                            throw new Exception();
+                }
+            }
+            context.getSquareByPosition(x, y).addView(toBePlaced);
+        }
+
+
+
+    }
+
+    public static ImageView searchPiece (List<ImageView> pieces, String string) {
+        for (ImageView imageView : pieces) {
+            if (((String) imageView.getTag()).equals(string)) {
+                return imageView;
+            }
+        }
     }
 
 }
